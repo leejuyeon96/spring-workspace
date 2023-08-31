fa674c46233fd4ffb8c5a9abbdea406bcb6beff2
leejuyeon96 <133620354+leejuyeon96@users.noreply.github.com>
2023-08-31 17:53:18 +0900

    05_MVC Board

--- 05_MVC_Board/src/test/java/com/kh/mvc/MyBatisUnitTest.java
+++ 05_MVC_Board/src/test/java/com/kh/mvc/MyBatisUnitTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.kh.mvc.model.vo.Board;
+import com.kh.mvc.model.vo.Criteria;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
@@ -25,43 +26,83 @@ public class MyBatisUnitTest {
 			r = Resources.getResourceAsReader("mybatis-config.xml");
 			SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(r);
 			return factory.openSession();
-			
+
 		} catch (IOException e) {
-			
+
 			e.printStackTrace();
 		}
 		return null;
 	}
-	
+
 	@Test
 	public void insertTest() {
 		SqlSession session = getSession();
-		
+
 		Board board = new Board();
 		board.setTitle("지우야");
 		board.setContent("눕지마");
 		board.setWriter("알겟지");
-		
+
 		System.out.println("db before :: " + board.getNo());
 		int result = session.insert("board.insert", board);
-		
-		if(result > 0) {
+
+		if (result > 0) {
 			System.out.println(result + "개 게시글 추가!");
 			session.commit();
 		}
 		System.out.println("db after :: " + board.getNo());
-		
+
 		System.out.println("===============================");
-		
+
 	}
-	
+
 	@Test
 	public void selectAllTest() {
 		SqlSession session = getSession();
-		List<Board> list = session.selectList("board.selectAll");
+		Criteria cri = new Criteria();
+
+		List<Board> list = session.selectList("board.selectAll", cri);
 		System.out.println(list);
 		System.out.println(list.size());
 		System.out.println("======================================");
-		
+
 	}
+
+	@Test
+	public void selectTest() {
+		SqlSession session = getSession();
+		Board board = session.selectOne("board.select", 10);
+		System.out.println(board);
+		System.out.println("==========================");
+	}
+
+	@Test
+	public void updateTest() {
+		SqlSession session = getSession();
+		Board board = new Board();
+		board.setNo(10);
+		board.setTitle("테스트 수정");
+		board.setContent("테스트에서 수정중");
+		int result = session.update("board.update", board);
+		if (result > 0) {
+			System.out.println(result + "개 게시글 수정!");
+			session.commit();
+		}
+	}
+
+	@Test
+	public void deleteTest() {
+		SqlSession session = getSession();
+		int result = session.delete("board.delete", 11);
+
+		if (result > 0) {
+
+			System.out.println(result + "개 게시글 삭제!");
+			session.commit();
+
+		}
+
+		System.out.println("===================================");
+	}
+
 }
